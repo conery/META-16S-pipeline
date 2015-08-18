19fcba4b03a734d2af0b26a564f36a6321cd830c
conery <conery@cs.uoregon.edu>
2015-08-18 15:30:52 -0700

    New functions in common.py: * argument parser initialized with common arguments * add messages to log table Clean up db_setup, have it call new common functions.

--- scripts/common.py
+++ scripts/common.py
@@ -1,4 +1,42 @@
-# A place to put code used by two or more scripts....
+###
+#
+#  Shared utilities used by scripts in the META 16S Analysis Pipeline
+#
+###
+
+# Each script requires the name of the project database as a positional argument
+# and requires a --force argument to overwrite an existing table.  This function
+# initializes an argument parser with common arguments, adds script-specific
+# arguments, and calls the argument parser.
+#
+#   desc:   script description to print as part of the help message
+#   specs:  a list of tuples with argument names and argument specs
+#   epi:    a second string to print in the help message
+
+import argparse
+
+def init_api(desc, specs, epi=None):
+    parser = argparse.ArgumentParser(description=desc, epilog=epi)
+    parser.add_argument('dbname', help='the name of the project database file')
+    parser.add_argument('--force', action='store_true', help='[GUI:flag::T] re-initialize an existing database')
+    
+    for x in specs:
+        parser.add_argument('--'+x[0], **x[1])
+        
+    args = parser.parse_args()
+    return args
+    
+###
+# Add a message to the log table.  Arguments are a reference to the database,
+# the type of event and a longer description
+
+import sys
+import os
+
+def record_metadata(db, event, message):
+    app = os.path.basename(sys.argv[0])
+    db.execute("INSERT INTO log VALUES (DATETIME('NOW'), ?, ?, ?)", (app, event, message))
+    
 
 ###
 # Create a map that associates a defline with the sequence ID in the 
--- scripts/db_setup.py
+++ scripts/db_setup.py
@@ -1,44 +1,47 @@
 #! /usr/bin/env python3
 
-# Load barcodes and other auxiliary info into a SQLite database for a PIP/NGS analysis pipeline.
+# Create a new SQLite database for a PIP/NGS analysis pipeline.
 
 # John Conery / Kevin Xu Junjie
 # University of Oregon
-# 2014-04-15
 
 # Usage:
 #
-#    db_setup.py [--force] dbname --info experiment
+#    db_setup.py dbname [--force] --data samples.csv [--primers primers.csv]
 #
-# Add sequence information to the database file with the specified name.  The --info argument
-# is the name of a file with barcodes, index sequences, and other information 
-# about the sequences.  By default the script checks to make sure the database 
-# does not exist already; use --force to re-initialize an existing database.
+# The required arguments are the database name and --data, which specifies a CSV
+# file that contains the names of the samples and the FASTQ files containing the
+# data for each sample.  
+#
+# If the data includes primers or offsets they can be specified in a second TSV file.  
+#
+# Use --force to re-initialize an existing database.
+#
+# Another optional argument, --message, can be used to add a project description
+# message to the log table.
 
 import sqlite3
 import argparse
 import os.path
 import sys
 
-# Strings in this list are names of items from the info file; plural forms
-# will be names of tables in the database
-
-items = ['primer', 'offset']
-
-# SQL command templates
-
-create_table = "CREATE TABLE {}s ( pair_end INTEGER, sequence TEXT )"
-drop_table = "DROP TABLE IF EXISTS {}s"
-insert = "INSERT INTO {}s VALUES (?, ?)"
+from common import *
 
 ### 
-# Helpers
+# Create the metadata table
 
 def init_log(db):
     "Create the log table, insert initial log messages"
     db.execute("DROP TABLE IF EXISTS log")
     db.execute("CREATE TABLE log ( time timestamp, script text, event text, message text )")
     
+###
+# Initialize the primers and offsets tables.  If the user specified a TSV file
+# with data for these tables add the records, otherwise leave the tables empty. 
+    
+create_table = "CREATE TABLE {}s ( pair_end INTEGER, sequence TEXT )"
+drop_table = "DROP TABLE IF EXISTS {}s"
+
 def make_table(db, tbl):
     "Initialize a table -- delete it if it exists already"
     db.execute(drop_table.format(tbl))
@@ -47,61 +50,57 @@ def make_table(db, tbl):
 def add_aux_info(db, args):
     "Create primer and offset tables, read the info file and populate the tables"
     
-    for tbl in items:
+    for tbl in ['primer', 'offset']:
         make_table(db, tbl)
     
-    if args.info is not None:
-        for line in open(args.info):
+    if args.primers is not None:
+        for line in open(args.primers):
             tbl, loc, seq = line.strip().split()
             if tbl in ['primer', 'offset']:
                 db.execute('INSERT INTO {}s VALUES (?, ?)'.format(tbl), (loc, seq))
             else:
                 print("Unknown item:", line.strip())
+
+###
+# Create the table that describes samples in the experiment.  Each record has
+# a sample ID and the names of the two FASTQ files with data for that sample.
             
 def add_sample_info(db, args):
     "Create the sample table with sample names and the names of their FASTQ files"
     
     db.execute('DROP TABLE IF EXISTS samples')
-    db.execute('CREATE TABLE samples (sample_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, r1_file TEXT, r2_file TEXT, barcode TEXT )')
+    db.execute('CREATE TABLE samples (sample_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, r1_file TEXT, r2_file TEXT)')
 
     for line in open(args.data):
         a = line.strip().split()
-        if len(a) < 4:
-            a.append(None)
-        db.execute('INSERT INTO samples (name, r1_file, r2_file, barcode) VALUES (?, ?, ?, ?)', tuple(a))
+        db.execute('INSERT INTO samples (name, r1_file, r2_file) VALUES (?, ?, ?)', tuple(a))
 
-### 
-# Set up command line arguments
 
-def init_api():
-    parser = argparse.ArgumentParser(
-        description="Initialize a SQLite3 database for a PIP/NGS analysis pipeline.",
-        epilog="If --force is not specified the program will print a message and exit if the database exists already."
-    )
-    parser.add_argument('dbname', help='the name of the SQLite database file')
-    parser.add_argument('--info', help='[GUI:path:info:<req>] path to file with primer and offset specs')
-    parser.add_argument('--data', required=True, help='[GUI:path:data:<req>] path to file with sample names and corresponding FASTQ file names')
-    parser.add_argument('--force', action='store_true', help='[GUI:flag::T] re-initialize an existing database')
-    parser.add_argument('--message', metavar='text', help='[GUI:str:Project description:] initial log message')
-    return parser.parse_args()
-    
 ###
 # Top level....
     
 if __name__ == "__main__":
-    args = init_api()
+    
+    args = init_api(
+        desc = "Initialize a SQLite3 database for a PIP/NGS analysis pipeline.",
+        epi = None,
+        specs = [
+            ('data',    { 'required' : True, 'metavar': 'x.csv', 'help' : '(required) CSV file with names of samples and their FASTQ files' } ),
+            ('primers', { 'metavar': 'x.csv', 'help' : '(optional) CSV file with primer and/or offset sequences' } ),
+            ('message', { 'metavar': '"text"', 'help' : 'project description to add to metadata'} ),
+        ]
+    )
 
     if os.path.exists(args.dbname) and not args.force:
         argparse.ArgumentParser.exit(1, 'Found existing database with that name; use --force to reinitialize')
-
+        
     db = sqlite3.connect(args.dbname)
     
     init_log(db)
     add_aux_info(db, args)
     add_sample_info(db, args)
     
-    db.execute("INSERT INTO log VALUES (DATETIME('NOW'), ?, ?, ?)", (sys.argv[0], 'start', ' '.join(sys.argv)) )
-    if args.message is not None:
-        db.execute("INSERT INTO log VALUES (DATETIME('NOW'), ?, ?, ?)", (sys.argv[0], 'message', args.message))
+    record_metadata(db, 'init', ' '.join(['data:', args.data, 'primers:', args.primers or 'None']))
+    record_metadata(db, 'desc', args.message)
 
     db.commit()
